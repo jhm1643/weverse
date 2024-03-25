a2306be103695611130103f51253b2a4b83b2937
조하명 <>
2024-03-25 23:08:08 +0900

    (feat)Initial commit

---
+++ src/main/java/com/weverse/WeverseApplication.java
@@ -0,0 +1,13 @@
+package com.weverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeverseApplication.class, args);
+    }
+
+}
